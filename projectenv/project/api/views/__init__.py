81f903fe4aa27a7f0a2788b99a97bd3ac3f7b1d8
temirbekbalabek <temirbek.balabek@mail.ru>
2019-05-12 01:11:35 +0600

    first

---
+++ projectenv/project/api/views/__init__.py
@@ -0,0 +1,4 @@
+from .fbv import gym_list, get_gym_detail
+from .cbv import CoachList, ClientList, ClientDetail
+from .generic_cbv import ShowTest, ShowSubscription, SubscriptionDetail, Feedback, AboutText
+from .auth import UserList, login, logout
